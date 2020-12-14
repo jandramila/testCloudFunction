ddc71d23e64cb6c875232023c3bab196e13c9ec6
Javier Ramila <jandramila@gmail.com>
2020-12-14 13:14:18 -0300

    Removes bulky code and simplifies API

--- index.js
+++ index.js
@@ -10,31 +10,25 @@ const { getSheet, getSheetConfig, setShiftRecords, setShiftTotal } = Spreadsheet
 
 const timezone = process.env.TIMEZONE;
 
-async function shiftFeedJob(shiftRange) {
-  // Config, Records & Spreadsheet object calls in parallel
-  const [{ DayShiftRows, TotalDayShiftRow }, timeseries, { rows }] = await Promise.all([
-    getSheetConfig(),
-    getAllFieldsByMachine(shiftRange),
-    getSheet(0, 'A1:HO50'),
-  ]);
-
-  const dateHumanRead = moment(shiftRange.start).tz(timezone).format('D-MMM-YY');
-
-  // Look for configured fields & machines
-  const total = setShiftRecords(DayShiftRows, dateHumanRead, rows, timeseries);
-  await setShiftTotal(TotalDayShiftRow, dateHumanRead, rows, total);
-
-  return {
-    machines: map(timeseries, (f, machine) => machine).join(', '),
-    total,
-  };
-}
-
 async function dayShiftFeedJob() {
   try {
     logger.info(`[DAY SHIFT JOB] Starting`);
     const shiftRange = getLastDayShiftRange();
-    const { machines, total } = await shiftFeedJob(shiftRange);
+
+    // Config, Records & Spreadsheet object calls in parallel
+    const [{ DayShiftRows, TotalDayShiftRow }, timeseries, { rows }] = await Promise.all([
+      getSheetConfig(),
+      getAllFieldsByMachine(shiftRange),
+      getSheet(0, 'A1:HO50'),
+    ]);
+
+    const dateHumanRead = moment(shiftRange.start).tz(timezone).format('D-MMM-YY');
+    const machines = map(timeseries, (f, machine) => machine).join(', ');
+
+    // Look for configured fields & machines
+    const total = setShiftRecords(DayShiftRows, dateHumanRead, rows, timeseries);
+    await setShiftTotal(TotalDayShiftRow, dateHumanRead, rows, total);
+
     logger.info(`[DAY SHIFT JOB] Completed - Records set for machines: ${machines}. Total: ${total}`);
   } catch (error) {
     logger.error(error);
@@ -45,42 +39,28 @@ async function nightShiftFeedJob() {
   try {
     logger.info(`[NIGHT SHIFT JOB] Starting`);
     const shiftRange = getLastNightShiftRange();
-    const { machines, total } = await shiftFeedJob(shiftRange);
+
+    // Config, Records & Spreadsheet object calls in parallel
+    const [{ NightShiftRows, TotalNightShiftRow }, timeseries, { rows }] = await Promise.all([
+      getSheetConfig(),
+      getAllFieldsByMachine(shiftRange),
+      getSheet(0, 'A1:HO50'),
+    ]);
+
+    const dateHumanRead = moment(shiftRange.start).tz(timezone).format('D-MMM-YY');
+    const machines = map(timeseries, (f, machine) => machine).join(', ');
+
+    // Look for configured fields & machines
+    const total = setShiftRecords(NightShiftRows, dateHumanRead, rows, timeseries);
+    await setShiftTotal(TotalNightShiftRow, dateHumanRead, rows, total);
+
     logger.info(`[NIGHT SHIFT JOB] Completed - Records set for machines: ${machines}. Total: ${total}`);
   } catch (error) {
     logger.error(error);
   }
 }
 
-async function shiftPeriodsJob(message) {
-  try {
-    const params = Buffer.from(message.data, 'base64').toString();
-    let [from, to, shift = null] = split(params, ',');
-    const logMessage = from === to ? from : `period ${from} - ${to}`;
-
-    [from, to] = map([from, to], (date) => moment.tz(date, 'YYYY-MM-DD', timezone));
-    shift = shift || SHIFT.day.name;
-    const upperShift = upperCase(shift);
-
-    console.log(`[${upperShift} SHIFT PERIOD JOB] Requested for ${logMessage}`);
-
-    for (const date = clone(from); !date.isAfter(to); date.add(1, 'd')) {
-      const shiftRange = getShiftRange(shift, { date });
-      const { machines, total } = await shiftFeedJob(shiftRange);
-
-      console.log(
-        `[${upperShift} SHIFT PERIOD JOB] Completed - Records set for machines: ${machines}. Total: ${total}`
-      );
-    }
-
-    console.log(`[${upperShift} SHIFT PERIOD JOB] Completed for ${logMessage}`);
-  } catch (error) {
-    console.error(error);
-  }
-}
-
 module.exports = {
   dayShiftFeedJob,
   nightShiftFeedJob,
-  shiftPeriodsJob,
 };
